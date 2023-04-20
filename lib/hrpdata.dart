57d45597b6606a0a57685907ad8fec92749d466c
Daniel Albert <daniel.albert@swan.de>
2023-04-20 15:44:05 +0200

    initial commit

---
+++ lib/hrpdata.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library hrpdata;
+
+export 'src/hrpdata_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
